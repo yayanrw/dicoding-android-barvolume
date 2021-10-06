e2efb025bed1de7bb1bbe8447b2e878046abd0c0
INBOOK_X1\yayan <yayanraw@gmail.com>
2021-10-06 08:45:57 +0700

    add orientation state

--- app/src/main/java/com/example/barvolume/MainActivity.kt
+++ app/src/main/java/com/example/barvolume/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.example.barvolume
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var edtWidth: EditText
